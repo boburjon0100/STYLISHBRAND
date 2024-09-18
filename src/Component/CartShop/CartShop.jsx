ce2e6d1fb9f480defe65376239177771507c8df6
boburjon0100 <boburjonqosimov418@gmail.com>
2024-09-18 15:51:33 +0500

    qqqq

--- src/Component/CartShop/CartShop.jsx
+++ src/Component/CartShop/CartShop.jsx
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CartShop.scss";
 import { BsBoxSeam, BsTelegram } from "react-icons/bs";
 import { IoMdHeartEmpty } from "react-icons/io";
 
 const CartShop = () => {
+  const images = [
+    "./images/cart11.png",
+    "./images/cart12.png",
+    "./images/cart13.png",
+    "./images/cart16.png",
+  ];
+  const [rightImage, setRightImage] = useState("./images/cart11.png");
+
+  function handleClick(imageUrl) {
+    setRightImage(imageUrl);
+  }
+
   return (
     <section className="cartshop">
       <div className="container">
         <div className="left">
           <div className="leftimg">
-          <div  className="heart">
-            <IoMdHeartEmpty />
+            <div className="heart">
+              <IoMdHeartEmpty />
             </div>
-            <img src="./images/cart11.png" alt="" />
-            
-            <img src="./images/cart12.png" alt="" />
-            <img src="./images/cart13.png" alt="" />
-            <img src="./images/cart15.png" alt="" />
+            {images &&
+              images.map((item, index) => {
+                return (
+                  <img
+                    src={item}
+                    alt=""
+                    key={index}
+                    onClick={() => handleClick(item)}
+                  />
+                );
+              })}
           </div>
           <div className="rightimg">
-            <img src="./images/cart11.png" alt="" />
+            <img src={rightImage} alt="" />
           </div>
         </div>
         <div className="right">
@@ -28,8 +46,8 @@ const CartShop = () => {
           <p
             style={{
               color: "#534C3E",
-              "padding-top": "15px",
-              "font-size": "16px",
+              paddingTop: "15px",
+              fontSize: "16px",
             }}
           >
             Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -39,8 +57,8 @@ const CartShop = () => {
           <p
             style={{
               color: "#8D8474",
-              "padding-top": "20px",
-              "font-size": "16px",
+              paddingTop: "20px",
+              fontSize: "16px",
             }}
           >
             Бренд
@@ -49,10 +67,10 @@ const CartShop = () => {
             style={{
               width: "140px",
               color: "black",
-              "padding-top": "5px",
-              "font-size": "18px",
-              "font-weight": "500",
-              "border-bottom": "1px solid black",
+              paddingTop: "5px",
+              fontSize: "18px",
+              fontWeight: "500",
+              borderBottom: "1px solid black",
               cursor: "pointer",
             }}
           >
@@ -64,7 +82,9 @@ const CartShop = () => {
                 <p>Цвета</p>
               </div>
               <div className="button">
-                <button style={{backgroundColor:"#E6C08B" , border:"none"}}>Черный</button>
+                <button style={{ backgroundColor: "#E6C08B", border: "none" }}>
+                  Черный
+                </button>
                 <button>Белый</button>
                 <button>Зеленый</button>
               </div>
@@ -72,34 +92,52 @@ const CartShop = () => {
             <div className="size1">
               <div className="top13">
                 <p>Размеры</p>
-                </div>
-                <div className="button2">
+              </div>
+              <div className="button2">
                 <button>S</button>
-                <button style={{backgroundColor:"#E6C08B" , border:"none"}}>M</button>
+                <button style={{ backgroundColor: "#E6C08B", border: "none" }}>
+                  M
+                </button>
                 <button>XL</button>
                 <button>XXL</button>
-                
               </div>
             </div>
           </div>
           <div className="price">
             <div className="narx">
-                <p  style={{
-                    "fontSize":"14px", 
-                    color:"#8D8474"
-            }}>Цена</p>
-                <p  style={{
-                    "fontSize":"28px", 
-                    color:"black"
-            }}>
+              <p
+                style={{
+                  fontSize: "14px",
+                  color: "#8D8474",
+                }}
+              >
+                Цена
+              </p>
+              <p
+                style={{
+                  fontSize: "28px",
+                  color: "black",
+                }}
+              >
                 $200
-                </p>
+              </p>
             </div>
-            <a href="#Oпepaтopoм"><button><BsTelegram/>Связаться c оператором</button></a>
+            <a href="#Oпepaтopoм">
+              <button>
+                <BsTelegram />
+                Связаться c оператором
+              </button>
+            </a>
           </div>
           <div className="dostavka">
-            <button><BsBoxSeam /> Доставка</button>
-            <p>Доставка по <span>Ташкенту</span> осуществляется за <span>1-3 дней</span>, A по <span>Узбекистану</span> за <span>1-3 дня</span></p>
+            <button>
+              <BsBoxSeam /> Доставка
+            </button>
+            <p>
+              Доставка по <span>Ташкенту</span> осуществляется за{" "}
+              <span>1-3 дней</span>, A по <span>Узбекистану</span> за{" "}
+              <span>1-3 дня</span>
+            </p>
           </div>
         </div>
       </div>
